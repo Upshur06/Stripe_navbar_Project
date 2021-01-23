9ac587f67fab45c4fc5931b7bd99d0f16c078169
Upshur06 <mupshur2005@gmail.com>
2021-01-23 02:12:49 -0500

    variables

---
+++ script-START.js
@@ -0,0 +1,8 @@
+const triggers = document.querySelectorAll('.cool > li');
+// console.log(triggers);
+
+const background = document.querySelectorAll('.dropdownBackground');
+// console.log(background);
+
+const nav = document.querySelectorAll('.top');
+// console.log(nav);
