0f06ffcf5a48ab55ef85ad223decde17fdab4e3f
Upshur06 <mupshur2005@gmail.com>
2021-01-23 02:22:52 -0500

    trigger handlers

--- script-START.js
+++ script-START.js
@@ -1,8 +1,19 @@
 const triggers = document.querySelectorAll('.cool > li');
 // console.log(triggers);
 
-const background = document.querySelectorAll('.dropdownBackground');
+const background = document.querySelector('.dropdownBackground');
 // console.log(background);
 
-const nav = document.querySelectorAll('.top');
+const nav = document.querySelector('.top');
 // console.log(nav);
+
+let handleEnter = function(){
+    console.log("ENTER!!!");
+}
+
+let handleLeave = function(){
+    console.log("LEAVE!!!");
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave ));
