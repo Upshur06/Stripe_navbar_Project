359606db4e29da007184846bcb4696dd9fa4bc9b
Upshur06 <mupshur2005@gmail.com>
2021-01-23 03:02:54 -0500

    coordnates for h and w

--- script-START.js
+++ script-START.js
@@ -10,10 +10,24 @@ const nav = document.querySelector('.top');
 let handleEnter = function(){
     this.classList.add('trigger-enter');
     setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+
+    const dropDown = this.querySelector('.dropdown');
+    const dropDownCoords = dropDown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+
+    const coords = {
+        height : dropDownCoords.height,
+        width : dropDownCoords.width
+    }
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
 }
 
 let handleLeave = function(){
     this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
